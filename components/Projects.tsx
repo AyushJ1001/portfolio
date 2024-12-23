ce09fbe844638ff6fa484f2388902a4f7df1626c
Ayush Juvekar <ayushjuvekar@gmail.com>
2024-12-23 07:45:17 -0600

    add yaycamp

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,24 +3,35 @@ export function Projects() {
     {
       title: "Anomaly Detection in Crowd Surveillance using Edge Computing",
       date: "2023 - 2024",
-      description: "The aim is to create an optimised and efficient machine learning system to detect anomalies in real time video surveillance feed by using Edge Computing and Neural Networks."
+      description:
+        "The aim is to create an optimised and efficient machine learning system to detect anomalies in real time video surveillance feed by using Edge Computing and Neural Networks.",
     },
     {
-      title: "Carbon Monoxide Concentration Monitoring System for Automating Air Filters",
+      title:
+        "Carbon Monoxide Concentration Monitoring System for Automating Air Filters",
       date: "2022 - 2023",
-      description: "Emerging Smart Computing and Informatics (ESCI), 2023 International Conference. The aim is to create an optimised and efficient machine learning system to detect anomalies in real time video surveillance feed by using Edge Computing and Neural Networks."
+      description:
+        "Emerging Smart Computing and Informatics (ESCI), 2023 International Conference. The aim is to create an optimised and efficient machine learning system to detect anomalies in real time video surveillance feed by using Edge Computing and Neural Networks.",
     },
     {
       title: "BookApp",
       date: "2022",
-      description: "A full stack typescript app for tagging books into a personal library for reading reference. Involves use of ExpressJS, MongoDB and React for UI."
+      description:
+        "A full stack typescript app for tagging books into a personal library for reading reference. Involves use of ExpressJS, MongoDB and React for UI.",
     },
     {
       title: "YelpCamp",
       date: "2021 - 2022",
-      description: "A full stack web app for organizing camping activities for yourself and your friends. It uses ExpressJS framework along with numerous libraries for geolocation data, authentication, database management and MongoDB as database."
-    }
-  ]
+      description:
+        "A full stack web app for organizing camping activities for yourself and your friends. It uses ExpressJS framework along with numerous libraries for geolocation data, authentication, database management and MongoDB as database.",
+    },
+    {
+      title: "YayCamp",
+      date: "2024",
+      description:
+        "Second iteration of YelpCamp, but with modern tools, including NextJS, TypeScript, TailwindCSS, Clerk (Authentication), T3 Stack",
+    },
+  ];
 
   return (
     <section id="projects" className="py-20">
@@ -37,6 +48,5 @@ export function Projects() {
         </div>
       </div>
     </section>
-  )
+  );
 }
-
