47e3f824f566e90ea292b4ce433846342a90f03a
Ayush Juvekar <ayushjuvekar@gmail.com>
2025-03-07 11:41:21 -0500

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'Ayush Juvekar - Portfolio',
-  description: 'Computer Science Graduate Student | Full Stack Developer | Machine Learning Enthusiast',
-}
+export const metadata: Metadata = {
+  title: "Ayush Juvekar - Portfolio",
+  description:
+    "Computer Science Graduate Student | Full Stack Developer | Machine Learning Enthusiast",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
